451ec497650b9b1d84083a9b292915ce8628d008
Purvansh Sharma <purvanshsharma6182gmail.com>
2025-02-20 21:45:37 +0530

    [Purvansh Sharma] Add. Create a JSON Object for a Student Problem Solved

---
+++ Day2/Day2/src/main/java/com/createjson/CreateJSONObject.java
@@ -0,0 +1,29 @@
+package com.createjson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CreateJSONObject {
+    public static void main(String[] args) {
+        // Creating a JSON object
+        JSONObject student = new JSONObject();
+
+        // Adding fields in the required order
+        student.put("name", "Aryan Sharma");
+        student.put("age", 20);
+
+        // Adding subjects at the end
+        JSONArray subjects = new JSONArray();
+        subjects.put("Mathematics");
+        subjects.put("Computer Science");
+        subjects.put("Physics");
+
+        student.put("subjects", subjects);
+
+        // Printing the JSON object
+        System.out.println(student.toString());
+    }
+}
+
+
+
